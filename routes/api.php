69a5646217491363f8b3a8a5a9bc4fde589aee06
Otim Isaac <isaacomega16@gmail.com>
2024-11-13 22:40:32 +0300

    add leads routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LeadController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    //leads routes
+    Route::post('/lead', [LeadController::class, 'store']);
+    Route::get('/lead', [LeadController::class, 'show']);
+    Route::get('/leads', [LeadController::class, 'index']);
+
+    //followup routes
+
+    
+});
