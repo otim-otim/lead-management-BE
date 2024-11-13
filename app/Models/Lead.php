e8c61670e41a658aecdfaccdbbe5c7d628296ced
Otim Isaac <isaacomega16@gmail.com>
2024-11-13 22:14:05 +0300

    setup lead model

--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -6,10 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Lead extends Model
 {
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+    ];
     public function followUps()
     {
         return $this->hasMany(FollowUp::class);
     }
 
-    publi
+    
 }
