7d02d5ced0731dd0009ca7f5905cc0f930517650
Otim Isaac <isaacomega16@gmail.com>
2024-11-13 22:36:32 +0300

    handle lead requests

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -2,51 +2,91 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\DTO\LeadCreateDTO;
 use App\Models\Lead;
 use Illuminate\Http\Request;
+use App\Services\LeadService;
 
 class LeadController extends Controller
 {
+    public function __construct(public LeadService $leadService)
+    {
+        
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        try {
+            $leads = $this->leadService->index();
+            return response()->json([
+                'success' => true,
+                'data' => $leads
+            ], 200);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json([
+                'success' => false,
+                'message' => $th->getMessage()
+            ],$th->getCode());
+        }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+    
+   
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validated = $request->validate([
+                'name' => ['required'],
+                'email' => ['required','email','unique:leads'],
+                'phone' => ['required','numeric',  'unique:leads'],
+            ]);
+
+            $dto = LeadCreateDTO::fromRequest($request);
+            $lead = $this->leadService->create($dto);
+            return response()->json([
+                'success' => true,
+                'data' => $lead
+            ], 200);
+            
+        } catch (\Throwable $th) {
+            // throw $th;
+            return response()->json([
+                'success' => false,
+                'message' => $th->getMessage()
+            ], $th->getCode());
+
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Lead $lead)
+    public function show($id)
     {
-        //
+        try {
+            $lead = $this->leadService->show($id);
+            return response()->json([
+                'success' => true,
+                'data' => $lead
+            ], 200);
+            
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json([
+                'success' => false,
+                'message' => $th->getMessage()
+            ],$th->getCode());
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Lead $lead)
-    {
-        //
-    }
-
+   
     /**
      * Update the specified resource in storage.
      */
