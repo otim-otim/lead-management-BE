bf9ec8faac1ecf9aed33d2ccd40b4d285aa1e4ec
Otim Isaac <isaacomega16@gmail.com>
2024-11-13 22:16:22 +0300

    setup followup model

--- app/Models/FollowUp.php
+++ app/Models/FollowUp.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class FollowUp extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function lead()
+    {
+        return $this->belongsTo(Lead::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
