23f2893de9c42bad091ddbe20a41208ba789e994
Otim Isaac <isaacomega16@gmail.com>
2024-11-13 22:16:57 +0300

    setup followup migration

--- database/migrations/2024_11_13_183951_create_follow_ups_table.php
+++ database/migrations/2024_11_13_183951_create_follow_ups_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Enums\FollowUpStatusEnum;
 
 return new class extends Migration
 {
@@ -13,6 +14,10 @@ return new class extends Migration
     {
         Schema::create('follow_ups', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id',)->constrained('users',)->onDelete('cascade');
+            $table->foreignId('lead_id')->constrained('leads',)->onDelete('cascade');
+            $table->timestamp('scheduled_at');
+            $table->enum('status', FollowUpStatusEnum::toArray())->default(FollowUpStatusEnum::PENDING->name);
             $table->timestamps();
         });
     }
