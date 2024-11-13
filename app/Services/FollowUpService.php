c378d50c1d06dafc8fb7fb82ab03357edd02fd63
Otim Isaac <isaacomega16@gmail.com>
2024-11-13 23:09:47 +0300

    add followup service

---
+++ app/Services/FollowUpService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Lead;
+use App\Models\FollowUp;
+use Illuminate\Support\Facades\DB;
+use App\Http\DTO\FollowUpCreateDTO;
+use App\Http\DTO\FollowUpUpdateDTO;
+use App\Http\Resources\FollowUpResource;
+
+class FollowUpService
+{
+    public function index(){
+        try {
+            $followups = FollowUp::latest()->get();
+            return FollowUpResource::collection($followups);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    public function store(FollowUpCreateDTO $dto){
+        try {
+            $followup = FollowUp::create($dto->toArray());
+            return new FollowUpResource($followup);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+
+    }
+
+    public function show($id){
+        try {
+            $followup = FollowUp::find($id);
+            return new FollowUpResource($followup);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    public function update(FollowUpUpdateDTO $dto, $id){
+        try {
+            $followup = FollowUp::findOrFail($id);
+            $followup->update($dto->toArray());
+            return new FollowUpResource($followup);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+}
