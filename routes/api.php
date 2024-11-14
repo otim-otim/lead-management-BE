b694d7296b34659234ae28f210edf1cfbfc177ac
Otim Isaac <isaacomega16@gmail.com>
2024-11-14 13:47:30 +0300

    add followup routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LeadController;
+use App\Http\Controllers\FollowUpController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -14,11 +15,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     //leads routes
-    Route::post('/lead', [LeadController::class, 'store']);
-    Route::get('/lead', [LeadController::class, 'show']);
+    Route::post('/leads', [LeadController::class, 'store']);
+    Route::get('/leads/{id}', [LeadController::class, 'show']);
     Route::get('/leads', [LeadController::class, 'index']);
 
     //followup routes
+
+    Route::post('/followups', [FollowUpController::class, 'store']);
+    Route::get('/followups/{id}', [FollowUpController::class, 'show']);
+    Route::get('/followups', [FollowUpController::class, 'index']);
+    Route::patch('/followups/{id}/{status}', [FollowUpController::class, 'updateFollowUpStatus']);
+
 
     
 });
