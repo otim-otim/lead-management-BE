57cf6a8c7d0e73750592f7693356f1941d1c7187
Otim Isaac <isaacomega16@gmail.com>
2024-11-14 12:44:34 +0300

    update followupstatus changed event api

--- app/Events/FollowUpStatusChanged.php
+++ app/Events/FollowUpStatusChanged.php
@@ -4,34 +4,35 @@ namespace App\Events;
 
 use App\Models\FollowUp;
 use Illuminate\Broadcasting\Channel;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
 
-class FollowUpStatusChanged
+class FollowUpStatusChanged implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $followUp;
+    public $oldStatus;
+    public $newStatus;
 
     /**
      * Create a new event instance.
      */
-    public function __construct(public FollowUp $followUp, $oldStatus)
+    public function __construct(FollowUp $followUp, string $oldStatus, string $newStatus)
     {
-        //
+        $this->followUp = $followUp;
+        $this->oldStatus = $oldStatus;
+        $this->newStatus = $newStatus;
     }
 
     /**
      * Get the channels the event should broadcast on.
-     *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
      */
-    public function broadcastOn(): array
+    public function broadcastOn(): PrivateChannel
     {
-        return [
-            new PrivateChannel('follow-up-updates'),
-        ];
+        return new PrivateChannel('follow-up-updates');
     }
 }
