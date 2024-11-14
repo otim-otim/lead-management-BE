34672d25c5e6c355f283d6b1d5cff9d000d6b73b
Otim Isaac <isaacomega16@gmail.com>
2024-11-14 11:35:17 +0300

    store a follow up

--- app/Http/Controllers/FollowUpController.php
+++ app/Http/Controllers/FollowUpController.php
@@ -2,33 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\DTO\FollowUpCreateDTO;
 use App\Models\FollowUp;
+use App\Services\FollowUpService;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class FollowUpController extends Controller
 {
+
+    public function __construct(public FollowUpService $followUpService)
+    {
+        
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        try {
+            $followups = $this->followUpService->index();
+            return response()->json([
+                'success' => true,
+                'data' =>$followups
+            ]);
+        } catch (\Throwable $th) {
+            // throw $th;
+            return response()->json([
+                'success' => false,
+                'message' => $th->getMessage()
+            ],$th->getCode());
+        }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+   
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validated = $request->validate([
+                'lead_id' => ['required',Rule::exists('leads','id')],
+                'user_id' => ['required',Rule::exists('users','id')],
+                'scheduled_at' => ['required','date:Y-m-d','after:today',],
+            ]);
+
+            $dto = FollowUpCreateDTO::fromRequest($request);
+            $follow_up = $this->followUpService->store($dto);
+            return response()->json([
+                'success' => true,
+                'data' => $follow_up
+            ],201);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json([
+                'success' => false,
+                'message' => $th->getMessage()
+            ], $th->getCode());
+        }
     }
 
     /**
