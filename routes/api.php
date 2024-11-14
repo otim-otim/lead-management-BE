75f975e2aca84a27ad186e3cc46648e946d6593c
Otim Isaac <isaacomega16@gmail.com>
2024-11-14 14:27:48 +0300

    add login route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LeadController;
 use App\Http\Controllers\FollowUpController;
+use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,6 +13,8 @@ Route::get('/user', function (Request $request) {
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     //leads routes
