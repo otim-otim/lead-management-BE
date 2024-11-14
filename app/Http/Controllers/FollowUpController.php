0ea7d927684e8a7860661cdfd6b2b3ed843c7848
Otim Isaac <isaacomega16@gmail.com>
2024-11-14 11:48:19 +0300

    update a followup

--- app/Http/Controllers/FollowUpController.php
+++ app/Http/Controllers/FollowUpController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\DTO\FollowUpCreateDTO;
 use App\Models\FollowUp;
-use App\Services\FollowUpService;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use App\Enums\FollowUpStatusEnum;
+use App\Services\FollowUpService;
+use App\Http\DTO\FollowUpCreateDTO;
+use App\Http\DTO\FollowUpUpdateDTO;
+use Illuminate\Validation\Rules\Exists;
 
 class FollowUpController extends Controller
 {
@@ -89,9 +92,28 @@ class FollowUpController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, FollowUp $followUp)
+    public function update(Request $request, $id)
     {
-        //
+        try {
+            $validated = $request->validate([
+                'user_id' => ['nullable', Rule::exists('users', 'id')],
+                'scheduled_at' => ['nullable', 'date:Y-m-d', 'after:today'],
+                'status' => ['nullable', Rule::in(FollowUpStatusEnum::toArray())] 
+            ]);
+            $dto = FollowUpUpdateDTO::fromRequest($request);
+            $follow_up = $this->followUpService->update($dto,$id);
+            
+            return response()->json([
+                'success' => true,
+                'data' => $follow_up
+            ],200);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json([
+                'success' => false,
+                'message' => $th->getMessage()
+            ], $th->getCode());
+        }
     }
 
     /**
