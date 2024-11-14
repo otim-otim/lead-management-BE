e48e3335cb3402cb0abe4ec0f1baad6272407b44
Otim Isaac <isaacomega16@gmail.com>
2024-11-14 13:22:13 +0300

    refactor channel to briadcast to user assigned to missed followup

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Broadcast;
+use App\Models\FollowUp;
 use App\Enums\UserRoleEnum;
+use App\Enums\FollowUpStatusEnum;
+use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
@@ -10,9 +12,20 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
 
 
 
-Broadcast::channel('follow-up-updates', function ($user) {
-    return in_array($user->role, [
+
+
+Broadcast::channel('follow-up-updates.{followUpId}', function ($user, $followUpId) {
+    // Check if the user has the correct role (ADMIN or SALESMANAGER)
+    $hasRole = in_array($user->role, [
         UserRoleEnum::ADMIN->value,
         UserRoleEnum::SALESMANAGER->value,
     ]);
+
+    // Check if the user is assigned to the particular FollowUp
+    $isAssigned = FollowUp::where('id', $followUpId)
+                          ->where('user_id', $user->id) 
+                          ->where('status', FollowUpStatusEnum::MISSED->value) 
+                          ->exists();
+
+    return $hasRole || $isAssigned;
 });
